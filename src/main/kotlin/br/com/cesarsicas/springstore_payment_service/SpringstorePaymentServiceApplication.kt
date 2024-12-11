2fcf63ea185d99707878f6e76ab414d7f2d15bdc
cesarsicas <7783112+cesarsicas@users.noreply.github.com>
2024-12-11 18:31:44 -0300

    First, eureka client already working

---
+++ src/main/kotlin/br/com/cesarsicas/springstore_payment_service/SpringstorePaymentServiceApplication.kt
@@ -0,0 +1,11 @@
+package br.com.cesarsicas.springstore_payment_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringstorePaymentServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringstorePaymentServiceApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/cesarsicas/springstore_payment_service/SpringstorePaymentServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.cesarsicas.springstore_payment_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringstorePaymentServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
